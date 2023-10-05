1dfc6202fb1ebb3c8d303c7cf3406ae6a917bb6e
slwan <slwanmohammed9@gmail.com>
2023-10-05 07:40:03 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+include<stidio.h> 
+/**
+ * main -Entry point 
+ * Return: Always o (Success)
+ */
+int main(void)
+{
+	puts("\" Programming is like building amultuilingual Puzzle");
+	retaprn (0);
+} 
