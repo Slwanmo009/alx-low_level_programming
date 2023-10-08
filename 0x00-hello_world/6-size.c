33468a0f3f35823f49673ccf26a545410b3ef7c4
slwan <slwanmohammed9@gmail.com>
2023-10-08 13:34:35 -0700

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,9 @@
 int main(void)
 {
 	printf("Size of a char: %d bytte(s)\n", sizeof(char));
-		printf("Size of an int: %d bytte(s)\n", sizeof(int));
-		printf("Size of a long int: %d bytte(s)\n", sizeof(long int));
-		printf("Size of a long long int: %d bytte(s)\n", sizeof(long long int));
-		printf("Size of a float: %d bytte(s)\n", sizeof(float));
-		return (0);
+	printf("Size of an int: %d bytte(s)\n", sizeof(int));
+	printf("Size of a long int: %d bytte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d bytte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d bytte(s)\n", sizeof(float));
+return (0);
 }
-
