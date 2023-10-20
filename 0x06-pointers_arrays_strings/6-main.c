9877edea9ffe495c607fad066f54b27fcf814af0
slwan <slwanmohammed9@gmail.com>
2023-10-20 13:39:47 -0700

    go c

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	char str[] = "Expect the best.Prepare for the worst.Capitalize on what comes\n
-		hello world! hello-world 0123456hello world\t
-		hello world.hello world\n";
+	char str[] = "Expect the best.Prepare for the worst.Capitalize on whatcomes\n"
+	"nhello world! hello-world 0123456hello world.\t"
+	"hello world.hello world.\n";
 	char *ptr;
 
 	ptr = cap_string(str);
