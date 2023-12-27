5bfa9b8f09f3d5af7fe0c9a6fd5db7c8cdbf92a4
slwan <slwanmohammed9@gmail.com>
2023-12-27 11:38:34 -0800

    mmm

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - print sing of number.
+ * Return: 0 if number less than 0
+ *          1 if zero -1 if 1.
+ */
+int print_sign(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('\n');
+	return (0);
+}
