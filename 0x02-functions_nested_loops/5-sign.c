c8943cd469c46ee456e2131d95a16e8e94e7d886
slwan <slwanmohammed9@gmail.com>
2023-12-28 11:53:12 -0800

    go

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,19 +6,19 @@
  */
 int print_sign(int n)
 {
-	int n;
+
 
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n = 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else if
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
