267f5a7bdbfaa7fcf78c1c05b49611217fced827
slwan <slwanmohammed9@gmail.com>
2023-12-29 01:05:08 -0800

    go

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - computes the absolute value of an integer.
+ * Return: 0
+ */
+int _abs(int)
+{
+	int n;
+
+	_putchar(n);
+	if (n < 0)
+		n = (2*n + n);
+	return(0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef main_h
 #define main_h
+int print_sign(int n);
 int _putchar(char);
 void print_alphabet(void);
 int _isalpha(int c);
