e06ee80a0b1010e745f0f320ef7c6e6116a8ded3
Slwanmo009 <slwanmohammed9@gmail.com>
2024-02-05 07:57:54 -0800

    kk

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints a linked listes
+ * @h: pointer to firest node
+ * Return: size of list
+ */
+size_t print_listint(const listint_t *h)
+{
+        size_t i = 0;
+
+        while (h)
+        {
+
+                h = h->next;
+                i++;
+        }
+        return (i);
+}
