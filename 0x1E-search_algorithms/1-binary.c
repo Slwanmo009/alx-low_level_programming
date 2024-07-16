636c085c77701c0e97d18f8f6a0a14323e27795b
slwanmo009 <slwanmohammed9@gmail.com>
2024-07-16 14:48:54 -0700

    ppo

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints the current array being searched.
+ * @array: Pointer to the first element of the array to search in.
+ * @left: The starting index of the subarray to print.
+ * @right: The ending index of the subarray to print.
+ */
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i < right)
+			printf(", ");
+	}
+	printf("\n");
+}
+/**
+ * binary_search - Searches for a value in a sorted array of int
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in the array.
+ * @value: Value to search for.
+ *
+ * Return: The index where value is located, or -1 if not found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left, right, mid;
+
+	if (array == NULL)
+		return (-1);
+	left = 0;
+	right = size - 1;
+	while (left <= right)
+	{
+		print_array(array, left, right);
+		mid = (left + right) / 2;
+		if (array[mid] == value)
+			return (int)mid;
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,7 @@
+# Linear Search Algorithm
+
+This project implements the linear search algorithm in C. The function searches for a value in an array of integers and returns the first index where the value is located.
+
+## Function Prototype
+```c
+int linear_search(int *array, size_t size, int value);
